9a42b157be2af224b2a00538dea65725041151bd
Jonathan Heady <32441523+jheady@users.noreply.github.com>
2025-09-14 18:09:10 -0700

    docker install via taskfile
    creating taskfiles with code to install docker repository and packages

---
+++ docker/add_docker_repo.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Add Docker Repository into an Ubuntu based system (including Mint)
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
